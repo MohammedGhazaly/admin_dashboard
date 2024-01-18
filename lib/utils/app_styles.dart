44f4c0ce7cdf603610e72bb569bc12a8b78ca303
MohammedGhazaly <warendragon67@gmail.com>
2024-01-18 22:54:32 +0200

    Added App styles

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class AppStyles {
+  static const TextStyle textStyleRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleMedium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle textStyleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleSemiBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleSemiBold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle textStyleSemiBold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle textStyleBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle textStyleMedium20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+  );
+}
