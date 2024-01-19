0cf7e25f20db5013ac875d97a2b9aa3bf4a301cc
MohammedGhazaly <warendragon67@gmail.com>
2024-01-19 18:21:06 +0200

    Added app assets

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,52 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar-1.svg
+  static const String imagesAvatar1 = "assets/images/avatar-1.svg";
+
+  /// Assets for imagesBalance
+  /// assets/images/balance.svg
+  static const String imagesBalance = "assets/images/balance.svg";
+
+  /// Assets for imagesCategory
+  /// assets/images/category.svg
+  static const String imagesCategory = "assets/images/category.svg";
+
+  /// Assets for imagesCreditCard
+  /// assets/images/credit-card.svg
+  static const String imagesCreditCard = "assets/images/credit-card.svg";
+
+  /// Assets for imagesExpanses
+  /// assets/images/expanses.svg
+  static const String imagesExpanses = "assets/images/expanses.svg";
+
+  /// Assets for imagesIncome
+  /// assets/images/income.svg
+  static const String imagesIncome = "assets/images/income.svg";
+
+  /// Assets for imagesInvestments
+  /// assets/images/investments.svg
+  static const String imagesInvestments = "assets/images/investments.svg";
+
+  /// Assets for imagesLogout
+  /// assets/images/logout.svg
+  static const String imagesLogout = "assets/images/logout.svg";
+
+  /// Assets for imagesSettings
+  /// assets/images/settings.svg
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  /// Assets for imagesStatistics
+  /// assets/images/statistics.svg
+  static const String imagesStatistics = "assets/images/statistics.svg";
+
+  /// Assets for imagesTransactions
+  /// assets/images/transactions.svg
+  static const String imagesTransactions = "assets/images/transactions.svg";
+
+  /// Assets for imagesWallet
+  /// assets/images/wallet.svg
+  static const String imagesWallet = "assets/images/wallet.svg";
+}
+
