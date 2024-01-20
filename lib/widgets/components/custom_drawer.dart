20de289a61fc0cb48ed0caa5ced1c22bb6450f48
MohammedGhazaly <warendragon67@gmail.com>
2024-01-20 17:12:47 +0200

    Created active/inactive drawer item

--- lib/widgets/components/custom_drawer.dart
+++ lib/widgets/components/custom_drawer.dart
@@ -4,16 +4,22 @@ import 'package:admin_dashboard/widgets/components/drawer_item.dart';
 import 'package:admin_dashboard/widgets/components/profile_picture_list_tile.dart';
 import 'package:flutter/material.dart';
 
-class CustomDrawer extends StatelessWidget {
+class CustomDrawer extends StatefulWidget {
   const CustomDrawer({super.key});
 
-  static const List<DrawerItemModel> drawerItems = [
+  @override
+  State<CustomDrawer> createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> {
+  final List<DrawerItemModel> drawerItems = const [
     DrawerItemModel(title: "Dashboard", image: Assets.imagesCategory),
     DrawerItemModel(title: "My Transaction", image: Assets.imagesTransactions),
     DrawerItemModel(title: "Statistics", image: Assets.imagesStatistics),
     DrawerItemModel(title: "Wallet Account", image: Assets.imagesWallet),
     DrawerItemModel(title: "My Investments", image: Assets.imagesInvestments),
   ];
+  int selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,15 +31,27 @@ class CustomDrawer extends StatelessWidget {
             email: "demo@gmail.com",
           ),
           ListView.builder(
-              itemCount: drawerItems.length,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(top: 10),
-                  child: DrawerItem(drawerItemModel: drawerItems[index]),
-                );
-              })
+            itemCount: drawerItems.length,
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: InkWell(
+                  onTap: () {
+                    if (selectedIndex != index) {
+                      selectedIndex = index;
+                      setState(() {});
+                    }
+                  },
+                  child: DrawerItem(
+                    drawerItemModel: drawerItems[index],
+                    isActive: selectedIndex == index,
+                  ),
+                ),
+              );
+            },
+          )
         ],
       ),
     );
--- lib/widgets/components/drawer_item.dart
+++ lib/widgets/components/drawer_item.dart
@@ -5,15 +5,65 @@ import 'package:flutter_svg/svg.dart';
 
 class DrawerItem extends StatelessWidget {
   final DrawerItemModel drawerItemModel;
-  const DrawerItem({super.key, required this.drawerItemModel});
+  final bool isActive;
+  const DrawerItem(
+      {super.key, required this.drawerItemModel, required this.isActive});
+
+  @override
+  Widget build(BuildContext context) {
+    return isActive
+        ? ActiveDrawerItem(drawerItemModel: drawerItemModel)
+        : InActiveDrawerItem(drawerItemModel: drawerItemModel);
+  }
+}
+
+class InActiveDrawerItem extends StatelessWidget {
+  const InActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: SvgPicture.asset(drawerItemModel.image),
+      trailing: AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        width: 0,
+        color: Colors.transparent,
+      ),
       title: Text(
         drawerItemModel.title,
         style: AppStyles.textStyleRegular16,
+      ),
+    );
+  }
+}
+
+class ActiveDrawerItem extends StatelessWidget {
+  const ActiveDrawerItem({
+    super.key,
+    required this.drawerItemModel,
+  });
+
+  final DrawerItemModel drawerItemModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItemModel.image),
+      trailing: AnimatedContainer(
+        duration: Duration(milliseconds: 500),
+        width: 3,
+        color: const Color(0xFF4EB7F2),
+      ),
+      title: Text(
+        drawerItemModel.title,
+        style: AppStyles.textStyleSemiBold16.copyWith(
+          color: const Color(0xFF4EB7F2),
+        ),
       ),
     );
   }
