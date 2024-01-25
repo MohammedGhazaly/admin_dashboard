7c43cdf4d313a7b58d261c85c321f766a98283c3
MohammedGhazaly <warendragon67@gmail.com>
2024-01-25 20:44:55 +0200

    Created transaction history item and started working on listview

---
+++ lib/widgets/components/transaction_item.dart
@@ -0,0 +1,37 @@
+import 'package:admin_dashboard/models/transaction_history_model.dart';
+import 'package:admin_dashboard/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({super.key, required this.transactionModel});
+  final TransactionModel transactionModel;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 0,
+      color: const Color(0xFFFAFAFA),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: ListTile(
+        title: Text(
+          transactionModel.name,
+          style: AppStyles.textStyleSemiBold16
+              .copyWith(color: const Color(0xFF064060)),
+        ),
+        subtitle: Text(
+          transactionModel.date,
+          style: AppStyles.textStyleRegular16
+              .copyWith(color: const Color(0xFFAAAAAA)),
+        ),
+        trailing: Text(
+          "\$${transactionModel.amount}",
+          style: AppStyles.textStyleSemiBold20.copyWith(
+              color: transactionModel.isWithdrawl == true
+                  ? const Color(0xFFF3735E)
+                  : const Color(0xFF7CD87A)),
+        ),
+      ),
+    );
+  }
+}
