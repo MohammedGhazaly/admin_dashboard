51b08b44e38a295c4e40c3bbc2c01471e63b9a1c
MohammedGhazaly <warendragon67@gmail.com>
2024-01-26 11:25:14 +0200

    created income section header

--- lib/widgets/layout/desktop_layout.dart
+++ lib/widgets/layout/desktop_layout.dart
@@ -1,10 +1,8 @@
+import 'package:admin_dashboard/utils/app_styles.dart';
 import 'package:admin_dashboard/widgets/components/all_expenses_and_quick_invoice_section.dart';
 import 'package:admin_dashboard/widgets/components/custom_drawer.dart';
-import 'package:admin_dashboard/widgets/components/my_card.dart';
+import 'package:admin_dashboard/widgets/components/expanses_drop_down.dart';
 import 'package:admin_dashboard/widgets/components/my_card_and_transaction_history.dart';
-import 'package:admin_dashboard/widgets/components/my_cards_page_view.dart';
-import 'package:admin_dashboard/widgets/components/my_cards_section.dart';
-import 'package:admin_dashboard/widgets/components/transaction_history.dart';
 import 'package:flutter/material.dart';
 
 class DesktopLayout extends StatelessWidget {
@@ -32,9 +30,45 @@ class DesktopLayout extends StatelessWidget {
         ),
         Expanded(
           flex: 2,
-          child: MyCardAndTransactionHistory(),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                MyCardAndTransactionHistory(),
+                IncomeSection(),
+              ],
+            ),
+          ),
         )
       ],
+    );
+  }
+}
+
+class IncomeSection extends StatelessWidget {
+  const IncomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(24),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(12)),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Income",
+                style: AppStyles.textStyleSemiBold16.copyWith(
+                  color: const Color(0xff064061),
+                ),
+              ),
+              ExpansesDropwDown()
+            ],
+          )
+        ],
+      ),
     );
   }
 }
